49ddd6d54177b683153d45a23878a527173245e4
MaiconDouglas-dev <maicon.timot8@gmail.com>
2026-03-17 15:24:29 -0300

    Commit inicial

---
+++ src/main/java/com/tmrs/costumer/bff/TmrsCostumerBffApplication.java
@@ -0,0 +1,13 @@
+package com.tmrs.costumer.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmrsCostumerBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmrsCostumerBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmrs/costumer/bff/TmrsCostumerBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmrs.costumer.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmrsCostumerBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
